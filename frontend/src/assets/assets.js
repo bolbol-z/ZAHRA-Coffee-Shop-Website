1d613212d12b0edc68c47e881a6b3cec51ecda6d
Bilel Zghal <bilelzghal.bi@gmail.com>
2025-08-21 20:27:27 +0100

    navbar

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,21 @@
+﻿import bg from './bg.png';
+import brochure from './brochure.png';
+import IMG_2430 from './IMG_2430.jpg';
+import meal from './meal.png';
+import newProject5 from './New Project-5.png';
+import reserved from './reserved.png';
+import zahraLogo from './ZAHRA_LOGO.png';
+import downarrow from './down-arrow.png';
+
+const images = {
+  bg,
+  brochure,
+  IMG_2430,
+  meal,
+  newProject5,
+  reserved,
+  zahraLogo,
+  downarrow
+};
+
+export default images;
---
+++ frontend/src/compoments/Navbar.jsx/Navbar.jsx
@@ -0,0 +1,47 @@
+﻿import React, { useState } from 'react';
+import './Navbar.css';
+import images from '../../assets/assets';
+
+const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  return (
+    <div className='navbar'>
+      {/* 1. القائمة على اليسار */}
+      <ul className="navbarmenu">
+        <li>Menu Principal</li>
+        <li>Réserver Maintenant</li>
+        <li className="dropdown-toggle">
+          <span>À Propos</span>
+          <img
+            src={images.downarrow}
+            alt="Dropdown"
+            className="dropdown-icon"
+            onClick={toggleDropdown}
+          />
+          {isDropdownOpen && (
+            <ul className="dropdown-menu">
+              <li>Contactez-Nous</li>
+              <li>Promotions Spéciales</li>
+              <li>Nos Boissons</li>
+            </ul>
+          )}
+        </li>
+      </ul>
+
+      {/* 2. الشعار في المنتصف */}
+      <img src={images.zahraLogo} alt="Logo" className="lgo" />
+      
+      {/* 3. الزر على اليمين */}
+      <div className="navbar-right">
+        <button className="rserve">Réserver</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
